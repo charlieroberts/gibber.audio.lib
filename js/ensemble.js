0afee108da6e83e90a3fa82200ec68ab4fd7c418
charlieroberts <charlie@charlie-roberts.com>
2020-03-04 21:28:07 -0500

    stop method

--- js/ensemble.js
+++ js/ensemble.js
@@ -38,6 +38,15 @@ module.exports = function( Audio ) {
     
     ens.tidals = []
 
+    ens.stop = function() {
+      ens.tidals.forEach( t => t.stop() )
+      ens.__sequencers.forEach( t => t.stop() )
+    }
+    ens.start = function() {
+      ens.tidals.forEach( t => t.start() )
+      ens.__sequencers.forEach( t => t.start() )
+    }
+
     ens.tidal = (pattern,num=0) => {
       if( ens.tidals[ num ] !== undefined ) ens.tidals[ num ].stop()
 
@@ -45,6 +54,19 @@ module.exports = function( Audio ) {
         target:ens,
         key:'play',
         pattern
+      }).start()
+
+      return ens
+    }
+    ens.__sequencers = []
+
+    ens.seq = (values,timings,num=0,offset=0) => {
+      if( ens.__sequencers[ num ] !== undefined ) ens.__sequencers[ num ].stop()
+
+      ens.__sequencers[ num ] = Gibber.Seq({
+        target:ens,
+        key:'play',
+        values,timings,offset
       }).start()
 
       return ens
