b93fc1e4ee189611fa3c250e72e66f28353b63ed
charlieroberts <charlie@charlie-roberts.com>
2021-01-04 20:25:34 -0800

    adding multisampler meta info

--- js/instruments.js
+++ js/instruments.js
@@ -56,6 +56,9 @@ const Instruments = {
     Sampler:{
       methods:[ 'note','trigger', 'loadFile', 'loadBuffer' ],
     },
+    Multisampler:{
+      methods:[ 'note','trigger', 'pick', 'pickFile'], 
+    },
     Snare:{
       methods:[ 'note','trigger' ],
     },
