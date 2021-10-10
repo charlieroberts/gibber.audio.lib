a310da8d5bbb858e33021f5ae106437635d91250
charlieroberts <charlie@charlie-roberts.com>
2021-10-10 02:24:03 -0400

    stab preset

--- js/presets/complex_presets.js
+++ js/presets/complex_presets.js
@@ -3,7 +3,8 @@ module.exports = {
     bias:.35,
     gain:1,
     decay:1/5,
-    pregain:4
+    pregain:4,
+    description:'a short, clean sounding preset with a minimum of distortion/wavefolding.'
   },
   
   'perc': {
@@ -11,6 +12,24 @@ module.exports = {
     gain:1,
     decay:1/5,
     pregain:5,
+    description:'a short, clean sounding preset with a minimum of distortion/wavefolding.'
+  },
+
+  stab: {
+    waveform:'saw', 
+    decay:1/4, 
+    bias:.1, 
+    filterMult:0, 
+    cutoff:.8, 
+    Q:.15, 
+    pregain:10, 
+    postgain:.25, 
+    filterModel:2, 
+    saturation:50,
+    presetInit( audio ) {
+      this.fx.push( audio.effects.Distortion('earshred') )
+    },
+    description:'a short, heavily distorted and filtered sound. in addition to the standard Complex wavefolding, this preset also adds an additional Distortion effect (preset earsred).'
   }
 
 }
