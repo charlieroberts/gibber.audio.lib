e3c914ab3ab4c97551852af08317f585beacc77b
charlieroberts <charlie@charlie-roberts.com>
2021-10-10 02:22:37 -0400

    modulation for lush chorus preset

--- js/presets/chorus_presets.js
+++ js/presets/chorus_presets.js
@@ -6,14 +6,16 @@ module.exports = {
     slowGain:3.5,
     slowFrequency:1,
     presetInit: function( audio ) {
+      const gen = audio.Gen.ugens
       this.mod1 = audio.Gen.make( audio.Gen.ugens.cycle(.1) ).connect( this.fastFrequency )
+      //this.fastGain =  audio.Gen.make( gen.add( .425, gen.cycle(.1) ) )
       this.mod2 = audio.Gen.make( audio.Gen.ugens.cycle(.05) ).connect( this.slowGain )
+      //this.slowGain = audio.Gen.make( gen.add( 4.5, gen.cycle(.05) ) )
     }
   },
 
   warbly: {
     fastFrequency:4,
-    fastGain:.425,
     slowGain:3,
     slowFrequency:1,
     fastGain:1.5,
