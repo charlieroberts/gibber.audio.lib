70bd3eacf54a2406020bce2afdf563de0dde5fad
charlieroberts <charlie@charlie-roberts.com>
2021-10-10 02:34:23 -0400

    reverb + delay presets

---
+++ js/presets/delay_presets.js
@@ -0,0 +1,14 @@
+module.exports = {
+  '1/3' : {
+    time:1/3, feedback:.35, wetdry:1 
+  },
+  '1/6' : {
+    time:1/6, feedback:.35, wetdry:1
+  },
+  '1/8' : {
+    time:1/8, feedback:.35, wetdry:1
+  },
+  '1/9' : {
+    time:1/9, feedback:.35, wetdry:1
+  }
+}
---
+++ js/presets/reverb.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+  'space': {
+    roomSize:.985, 
+    dry:1 
+  }
+}
