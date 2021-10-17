7d9f03f4543bf1f6df8c80547bf8a10ca42cc39a
charlieroberts <charlie@charlie-roberts.com>
2021-10-16 23:00:22 -0400

    more delay presets

--- js/presets/delay_presets.js
+++ js/presets/delay_presets.js
@@ -10,5 +10,41 @@ module.exports = {
   },
   '1/9' : {
     time:1/9, feedback:.35, wetdry:1
+  },
+  '1/3.fb' : {
+    time:1/3, feedback:.85, wetdry:1 
+  },
+  '1/6.fb' : {
+    time:1/6, feedback:.85, wetdry:1
+  },
+  '1/8.fb' : {
+    time:1/8, feedback:.85, wetdry:1
+  },
+  '1/9.fb' : {
+    time:1/9, feedback:.85, wetdry:1
+  },
+  '1/3.dry' : {
+    time:1/3, feedback:.35, wetdry:.5 
+  },
+  '1/6.dry' : {
+    time:1/6, feedback:.35, wetdry:.5
+  },
+  '1/8.dry' : {
+    time:1/8, feedback:.35, wetdry:.5
+  },
+  '1/9.dry' : {
+    time:1/9, feedback:.35, wetdry:.5
+  },
+  '1/3.dry.fb' : {
+    time:1/3, feedback:.85, wetdry:.5 
+  },
+  '1/6.dry.fb' : {
+    time:1/6, feedback:.35, wetdry:.5
+  },
+  '1/8.dry.fb' : {
+    time:1/8, feedback:.85, wetdry:.5
+  },
+  '1/9.dry.fb' : {
+    time:1/9, feedback:.85, wetdry:.5
   }
 }
